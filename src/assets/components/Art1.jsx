adc4fdab9e119fd6b2afbb93d78baf015e1cacb2
laura.velez <lauravelez1991@hotmail.com>
2024-09-25 09:15:52 -0500

    Componentes imágenes

---
+++ src/assets/components/Art1.jsx
@@ -0,0 +1,9 @@
+import art1 from '../assets/img/Art1.jpg'
+
+export const Art1 = ({ props }) => {
+    return (
+        <img src={Art1} alt="Escultura"
+            className={props.className}/>
+    )
+ }
+
